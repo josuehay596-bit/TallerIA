2711f552aed517cf05eff4713bc48c2a8d0f180b
Josh <Josh@2.com>
2025-11-25 18:59:02 -0600

    Mi segundo commit: html: html

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 //suma dos numeros a + b
+function suma(a,b) {
+    return a + b;
+}
 
 
+function sumaEnteros(a,b){
+    if(!Number.isInteger(a) || !Number.isInteger(b)){
+        throw new Error('ambos parametrosdeben ser numero enteros');
+    }
+}
+    
